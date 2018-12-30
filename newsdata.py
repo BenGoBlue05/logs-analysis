bc3568fb746b43a5378feba137a3a8da7f8283f4
Ben Lewis <bplewis5@gmail.com>
2018-12-30 12:31:54 -0500

    update to abide by pep8

--- newsdata.py
+++ newsdata.py
@@ -8,7 +8,7 @@ divider = "----------------------------------------------------------"
 def most_popular_articles():
     query = '''
     SELECT articles.id, title, count(*) as num
-    FROM articles, log 
+    FROM articles, log
     WHERE log.path = concat('/article/', articles.slug)
     GROUP BY articles.id
     ORDER BY num DESC
@@ -25,7 +25,8 @@ def most_popular_authors():
     query = '''
         SELECT authors.id, authors.name, count(*) as num
         FROM authors, articles, log
-        WHERE authors.id = articles.author AND log.path = concat('/article/', articles.slug)
+        WHERE authors.id = articles.author
+        AND log.path = concat('/article/', articles.slug)
         GROUP BY authors.id
         ORDER BY num DESC
         LIMIT 3;
@@ -41,16 +42,18 @@ def days_with_errors_over_1_percent():
     query = '''
         SELECT day, rate
         FROM(
-            SELECT to_char(time, 'FMMonth DD, yyyy') AS day, avg(CASE WHEN status like '40%' THEN 1 ELSE 0 END) as rate
+            SELECT to_char(time, 'FMMonth DD, yyyy') AS day,
+            avg(CASE WHEN status like '40%' THEN 1 ELSE 0 END) as rate
             FROM log
             GROUP BY day
         ) as subq
         WHERE rate > 0.01
-        ORDER BY rate DESC 
+        ORDER BY rate DESC
     '''
 
     rates = execute_query(query)
-    res = "Days Where More Than 1% of Requests Lead to Errors\n{}".format(divider)
+    res = "Days Where More Than 1% of Requests Lead to Errors\n{}" \
+        .format(divider)
     for rate in rates:
         pct = "{:.1%}".format(rate[1])
         res += "\n{0} - {1} errors".format(rate[0], pct)
@@ -67,8 +70,10 @@ def execute_query(query):
 
 
 def print_result():
-    print("\n{0}\n\n{1}\n\n{2}\n".format(most_popular_articles(), most_popular_authors(),
-                                         days_with_errors_over_1_percent()))
+    articles = most_popular_articles()
+    authors = most_popular_authors()
+    days = days_with_errors_over_1_percent()
+    print("\n{0}\n\n{1}\n\n{2}\n".format(articles, authors, days))
 
 
 print_result()
